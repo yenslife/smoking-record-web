821038beeccd9499504ec6eea2c358fa147a514e
horseface1110 <oloomb6@gmail.com>
2025-09-11 21:21:59 +0800

    Change port

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ async def persons(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
